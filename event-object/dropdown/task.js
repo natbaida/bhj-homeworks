8d6e9967ea3e76a10a74268ab464d665722647e9
Natalia Baida <nnbaida@yandex.com>
2025-09-16 18:05:46 +0300

    done dropdown

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,17 @@
+document.querySelectorAll('.dropdown').forEach(dropdown => {
+    const value = dropdown.querySelector('.dropdown__value');
+    const list = dropdown.querySelector('.dropdown__list');
+    const links = dropdown.querySelectorAll('.dropdown__link');
+
+    value.addEventListener('click', () => {
+        list.classList.toggle('dropdown__list_active');
+    });
+
+    links.forEach(link => {
+        link.addEventListener('click', event => {
+            event.preventDefault();
+            value.textContent = link.textContent;
+            list.classList.remove('dropdown__list_active');
+        });
+    });
+});
